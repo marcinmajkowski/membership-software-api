4887f0027ee410c67b98eba09d4e6a0e8d36d32a
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-07-24 19:37:51 +0200

    Generate Spring application using start.spring.io

---
+++ src/main/java/com/marcinmajkowski/membershipsoftware/MembershipSoftwareApiApplication.java
@@ -0,0 +1,12 @@
+package com.marcinmajkowski.membershipsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipSoftwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipSoftwareApiApplication.class, args);
+	}
+}
